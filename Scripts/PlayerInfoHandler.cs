8c3b6c4859388382f573add562d78a9d6b16da9c
sumueldotjpg <samscoggins@outlook.com>
2024-01-24 23:39:39 +0000

    24/03/24 Done.

--- Scripts/PlayerInfoHandler.cs
+++ Scripts/PlayerInfoHandler.cs
@@ -6,6 +6,8 @@ public partial class PlayerInfoHandler : RichTextLabel
 {
 	[Export]
 	RichTextLabel playerLabel;
+
+	string GameDescription = "Welcome To Decription Domination!\nThe aim of this game is to take over the world by upgrading your virus to attack larger companies and buying NPCs to do some of the work for you.\nAs you progress through the game the detection bar on the right will slowly increase. But BEWARE if this bar reaches its maximum you will lose as the authorities have found you out.\nSo go forward and try and attack your first Point Of Interest(Genuine Solution)";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -15,8 +17,10 @@ public partial class PlayerInfoHandler : RichTextLabel
 	public override void _Process(double delta)
 	{
 		playerLabel.Text=
-		$"Stats:\n" +
 		$"Balance: {AllObjects.CurrentProfile.MoneyBalance}\n" +
-		$"Next POI's to unlock:";
+		$"Unlocked POIs: {AllObjects.CurrentProfile.UnlockedPOIs.Count}\n" +
+		$"Unlocked NPCs: {AllObjects.CurrentProfile.UnlockedNPCs.Count}\n" +
+		$"Unlocked Attacks: {AllObjects.CurrentProfile.UnlockedAttacks.Count}\n\n" +
+		GameDescription;
 	}
 }
